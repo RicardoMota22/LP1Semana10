59dc9aac10db72a3eed70a5a6b1aa880dd08260b
RicardoMota22 <ricardomota.artist@gmail.com>
2025-05-08 21:40:14 +0100

    coleções genericas

---
+++ IntCollections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntCollections
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[]values = { 1, 10, -30, 10, -5 };
+
+            // 1. List
+            List<int> list = new List<int>();
+
+            // 2. Stack
+            Stack<int> stack = new Stack<int>();
+
+            // 3. Queue
+            Queue<int> queue = new Queue<int>();
+
+            // 4. HashSet
+            HashSet<int> hashset = new HashSet<int>();
+        }
+    }
+}
