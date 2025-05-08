42cc2cb9331dbf3297ecead0b0e6ad1b7d331825
RicardoMota22 <ricardomota.artist@gmail.com>
2025-05-08 22:07:40 +0100

    add coleções genericas com valores imprimidos

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -20,6 +20,39 @@ namespace IntCollections
 
             // 4. HashSet
             HashSet<int> hashset = new HashSet<int>();
+
+            foreach (int value in values)
+            {
+                list.Add(value);
+                stack.Push(value);
+                queue.Enqueue(value);
+                hashset.Add(value);
+            }
+
+
+            //List
+            Console.WriteLine("List:");
+            foreach (int i in list)
+                Console.Write(i + " ");
+            Console.WriteLine();
+
+            //Stack
+            Console.WriteLine("Stack:");
+            foreach (int i in stack)
+                Console.Write(i + " ");
+            Console.WriteLine();
+
+            //Queue
+            Console.WriteLine("Queue:");
+            foreach (int i in queue)
+                Console.Write(i + " ");
+            Console.WriteLine();
+
+            //Hashset
+            Console.WriteLine("Hashset:");
+            foreach (int i in hashset)
+                Console.Write(i + " ");
+            Console.WriteLine();
         }
     }
 }
