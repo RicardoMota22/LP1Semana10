4267826133baa7b4a721d77816ca311a4ddba433
RicardoMota22 <ricardomota.artist@gmail.com>
2025-05-09 10:20:06 +0100

    add ex5, aceita 1 arg

---
+++ FilePower1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("just 1 arg");
+                return;
+            }
+
+            string filename = args[0];
+        }
+    }
+}
